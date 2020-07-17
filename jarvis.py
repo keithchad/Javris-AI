de08397e6414e58d673b2a4e499d16016c85ac24
keithchad <63049827+keithchad@users.noreply.github.com>
2020-07-17 16:12:21 +0300

    send email and chrome search

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,9 @@ import pyttsx3 # pip install pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia
+import smtplib
+import webbrowser as wb
+import os
 
 engine = pyttsx3.init()
 def speak(audio):
@@ -40,7 +43,7 @@ def wishme():
 
 def takeCommand():
     r = sr.Recognizer()
-    with sr.Microphone as source:
+    with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
@@ -57,6 +60,15 @@ def takeCommand():
         return"None"
     return query
 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls
+    server.login('chaddev10@gmail.com', '0718265385')
+    server.sendmail('chadev10@gmail.com', to , content)
+    server.close()
+
+
 if __name__ == "__main__":
     wishme()
     while True :
@@ -72,6 +84,21 @@ if __name__ == "__main__":
             result = wikipedia.summary(query, sentences=2)
             print(result)
             speak(result)
+        elif 'send email' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "joykinya02@gmail.com"
+                sendEmail(to, content)
+                speak("Email successfully sent!")
+            except Exception as e:
+                    print(e)
+                    speak("Unable to send Email") 
+        elif 'search in chrome' in query:
+            speak("What should i Search?")
+            chromepath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+            search = takeCommand().lower
+            wb.get(chromepath).open_new_tab(search+'.com')
         elif 'offline' in query:
             quit()
 
